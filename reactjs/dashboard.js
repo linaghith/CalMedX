548819cc0a4671b7019d73eb69a5020b3b1ff55a
Khoi Tran <tranlongkhoi@gmail.com>
2015-04-11 14:34:45 -0700

    container box

--- reactjs/dashboard.js
+++ reactjs/dashboard.js
@@ -48,53 +48,61 @@ var CMDashboard = React.createClass({
     };
 
     return (
+      
+      <div className="container">
 
-      <div id="page-wrapper" style={wrapperStyle}>
+        <div id="page-wrapper" style={wrapperStyle}>
 
-        <div className="row">
-			<h3>Marla Dixon 58, F, American
-                <span className="pull-right text-muted small">
-                    <small>
-                       <br>DOB:  12/12/1956</br>
-                       <br>MRN:  402500433</br>
-                    </small>
-                </span><br></br>
-            </h3>
+          <div className="row">
+              <h3>Marla Dixon 58, F, American
+                  <span className="pull-right text-muted small">
+                      <small>
+                         <br>DOB:  12/12/1956</br>
+                         <br>MRN:  402500433</br>
+                      </small>
+                  </span><br></br>
+              </h3>
+          </div>
+
+          <div className="row">
+                  <div className="col-lg-12"> <CBComplaints complaints={this.state.complaintsdata.complaints} /> </div>
+          </div>
+
+          <div className="row">
+            <h4>Current Vitals</h4>
+          </div>
+
+          <div className="row">
+              <div className="col-md-3 col-sm-6"> <CBBloodPressures bloodPressures={this.state.data.bloodPressures} /> </div>
+              <div className="col-md-3 col-sm-6"> <CBBodyTemperatures bodyTemperatures={this.state.data.bodyTemperatures} /> </div>
+              <div className="col-md-3 col-sm-6"> <CBBodyWeights bodyWeights={this.state.data.bodyWeights} /> </div>
+              <div className="col-md-3 col-sm-6"> <CBHeartRates heartRates={this.state.data.heartRates} /> </div>
+          </div>
+
+          <div className="row">
+              <div className="col-md-4"> <CBMedications medications={this.state.medicationsdata.medications} /> </div>
+              <div className="col-md-4"> <CBAllergies allergies={this.state.allergiesdata.allergies} /> </div>
+              <div className="col-md-4"> <CBProcedures procedures={this.state.proceduresdata.procedures} /> </div>
+          </div>
+
+          <div className="row">
+              <div className="col-md-4"> <CBConditions conditions={this.state.conditionsdata.conditions} /> </div>
+              <div className="col-md-4"> <CBImmunizations immunizations={this.state.immunizationsdata.immunizations} /> </div>
+              <div className="col-md-4"> <CBFamilyHistories familyHistories={this.state.familyhistorydata.familyHistories} /> </div>
+          </div>
+
+           <div className="row">
+              <div className="col-lg-3 col-md-6"> <CBLabTest labTests={this.state.labsdata.labTests} /> </div>
+          </div>
+   
+          <div className="row">
+            <div className="col-lg-12 col-md-6"> <CBPatientVisitSummaries patientVisitSummaries={this.state.patientvisitsummarydata.patientVisitSummaries} /> </div>
+            <div className="col-lg-12 col-md-6"> <CBPatientVisitHistories patientVisitHistories={this.state.patientvisithistorydata.patientVisitHistories} /> </div>
+          </div>
+
+
         </div>
-		<div className="row">
-            <div className="col-lg-12"> <CBComplaints complaints={this.state.complaintsdata.complaints} /> </div>
-		</div>
-		<div className="row">
-			<h4>Current Vitals</h4>
-		</div>
-		<div className="row">
-            <div className="col-md-3 col-sm-6"> <CBBloodPressures bloodPressures={this.state.data.bloodPressures} /> </div>
-            <div className="col-md-3 col-sm-6"> <CBBodyTemperatures bodyTemperatures={this.state.data.bodyTemperatures} /> </div>
-            <div className="col-md-3 col-sm-6"> <CBBodyWeights bodyWeights={this.state.data.bodyWeights} /> </div>
-            <div className="col-md-3 col-sm-6"> <CBHeartRates heartRates={this.state.data.heartRates} /> </div>
-		</div>
-
-        <div className="row">
-             <div className="col-lg-2 col-md-6"> <CBMedications medications={this.state.medicationsdata.medications} /> </div>
-            <div className="col-lg-2 col-md-6"> <CBAllergies allergies={this.state.allergiesdata.allergies} /> </div>
-            <div className="col-lg-2 col-md-6"> <CBProcedures procedures={this.state.proceduresdata.procedures} /> </div>
-            <div className="col-lg-2 col-md-6"> <CBConditions conditions={this.state.conditionsdata.conditions} /> </div>
-            <div className="col-lg-2 col-md-6"> <CBImmunizations immunizations={this.state.immunizationsdata.immunizations} /> </div>
-            <div className="col-lg-2 col-md-6"> <CBFamilyHistories familyHistories={this.state.familyhistorydata.familyHistories} /> </div>
-           
-        </div>
-         <div className="row">
- 			<div className="col-lg-3 col-md-6"> <CBLabTest labTests={this.state.labsdata.labTests} /> </div>
- 		</div>
- 
-		<div className="row">
-			<div className="col-lg-12 col-md-6"> <CBPatientVisitSummaries patientVisitSummaries={this.state.patientvisitsummarydata.patientVisitSummaries} /> </div>
-			<div className="col-lg-12 col-md-6"> <CBPatientVisitHistories patientVisitHistories={this.state.patientvisithistorydata.patientVisitHistories} /> </div>
-		</div>
-
-
       </div>
-
     );
   }
 });
--- reactjs_build/dashboard.js
+++ reactjs_build/dashboard.js
@@ -48,53 +48,61 @@ var CMDashboard = React.createClass({displayName: "CMDashboard",
     };
 
     return (
+      
+      React.createElement("div", {className: "container"}, 
 
-      React.createElement("div", {id: "page-wrapper", style: wrapperStyle}, 
+        React.createElement("div", {id: "page-wrapper", style: wrapperStyle}, 
 
-        React.createElement("div", {className: "row"}, 
-			React.createElement("h3", null, "Marla Dixon 58, F, American", 
-                React.createElement("span", {className: "pull-right text-muted small"}, 
-                    React.createElement("small", null, 
-                       React.createElement("br", null, "DOB:  12/12/1956"), 
-                       React.createElement("br", null, "MRN:  402500433")
-                    )
-                ), React.createElement("br", null)
-            )
-        ), 
-		React.createElement("div", {className: "row"}, 
-            React.createElement("div", {className: "col-lg-12"}, " ", React.createElement(CBComplaints, {complaints: this.state.complaintsdata.complaints}), " ")
-		), 
-		React.createElement("div", {className: "row"}, 
-			React.createElement("h4", null, "Current Vitals")
-		), 
-		React.createElement("div", {className: "row"}, 
-            React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBloodPressures, {bloodPressures: this.state.data.bloodPressures}), " "), 
-            React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBodyTemperatures, {bodyTemperatures: this.state.data.bodyTemperatures}), " "), 
-            React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBodyWeights, {bodyWeights: this.state.data.bodyWeights}), " "), 
-            React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBHeartRates, {heartRates: this.state.data.heartRates}), " ")
-		), 
+          React.createElement("div", {className: "row"}, 
+              React.createElement("h3", null, "Marla Dixon 58, F, American", 
+                  React.createElement("span", {className: "pull-right text-muted small"}, 
+                      React.createElement("small", null, 
+                         React.createElement("br", null, "DOB:  12/12/1956"), 
+                         React.createElement("br", null, "MRN:  402500433")
+                      )
+                  ), React.createElement("br", null)
+              )
+          ), 
 
-        React.createElement("div", {className: "row"}, 
-             React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBMedications, {medications: this.state.medicationsdata.medications}), " "), 
-            React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBAllergies, {allergies: this.state.allergiesdata.allergies}), " "), 
-            React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBProcedures, {procedures: this.state.proceduresdata.procedures}), " "), 
-            React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBConditions, {conditions: this.state.conditionsdata.conditions}), " "), 
-            React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBImmunizations, {immunizations: this.state.immunizationsdata.immunizations}), " "), 
-            React.createElement("div", {className: "col-lg-2 col-md-6"}, " ", React.createElement(CBFamilyHistories, {familyHistories: this.state.familyhistorydata.familyHistories}), " ")
-           
-        ), 
-         React.createElement("div", {className: "row"}, 
- 			React.createElement("div", {className: "col-lg-3 col-md-6"}, " ", React.createElement(CBLabTest, {labTests: this.state.labsdata.labTests}), " ")
- 		), 
- 
-		React.createElement("div", {className: "row"}, 
-			React.createElement("div", {className: "col-lg-12 col-md-6"}, " ", React.createElement(CBPatientVisitSummaries, {patientVisitSummaries: this.state.patientvisitsummarydata.patientVisitSummaries}), " "), 
-			React.createElement("div", {className: "col-lg-12 col-md-6"}, " ", React.createElement(CBPatientVisitHistories, {patientVisitHistories: this.state.patientvisithistorydata.patientVisitHistories}), " ")
-		)
+          React.createElement("div", {className: "row"}, 
+                  React.createElement("div", {className: "col-lg-12"}, " ", React.createElement(CBComplaints, {complaints: this.state.complaintsdata.complaints}), " ")
+          ), 
+
+          React.createElement("div", {className: "row"}, 
+            React.createElement("h4", null, "Current Vitals")
+          ), 
+
+          React.createElement("div", {className: "row"}, 
+              React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBloodPressures, {bloodPressures: this.state.data.bloodPressures}), " "), 
+              React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBodyTemperatures, {bodyTemperatures: this.state.data.bodyTemperatures}), " "), 
+              React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBBodyWeights, {bodyWeights: this.state.data.bodyWeights}), " "), 
+              React.createElement("div", {className: "col-md-3 col-sm-6"}, " ", React.createElement(CBHeartRates, {heartRates: this.state.data.heartRates}), " ")
+          ), 
+
+          React.createElement("div", {className: "row"}, 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBMedications, {medications: this.state.medicationsdata.medications}), " "), 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBAllergies, {allergies: this.state.allergiesdata.allergies}), " "), 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBProcedures, {procedures: this.state.proceduresdata.procedures}), " ")
+          ), 
+
+          React.createElement("div", {className: "row"}, 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBConditions, {conditions: this.state.conditionsdata.conditions}), " "), 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBImmunizations, {immunizations: this.state.immunizationsdata.immunizations}), " "), 
+              React.createElement("div", {className: "col-md-4"}, " ", React.createElement(CBFamilyHistories, {familyHistories: this.state.familyhistorydata.familyHistories}), " ")
+          ), 
+
+           React.createElement("div", {className: "row"}, 
+              React.createElement("div", {className: "col-lg-3 col-md-6"}, " ", React.createElement(CBLabTest, {labTests: this.state.labsdata.labTests}), " ")
+          ), 
+   
+          React.createElement("div", {className: "row"}, 
+            React.createElement("div", {className: "col-lg-12 col-md-6"}, " ", React.createElement(CBPatientVisitSummaries, {patientVisitSummaries: this.state.patientvisitsummarydata.patientVisitSummaries}), " "), 
+            React.createElement("div", {className: "col-lg-12 col-md-6"}, " ", React.createElement(CBPatientVisitHistories, {patientVisitHistories: this.state.patientvisithistorydata.patientVisitHistories}), " ")
+          )
 
 
+        )
       )
-
     );
   }
 });
