9adc2927b099e0d6697767da514b3318133fd545
GreatAmbersand <spoojalakshmi@gmail.com>
2015-04-13 19:48:44 -0700

    Adding collape for patient history

--- reactjs/patientVisitHistory.js
+++ reactjs/patientVisitHistory.js
@@ -1,15 +1,35 @@
 var CBPatientVisitHistories = React.createClass({
+  getInitialState: function() {
+    return {liked: false};
+  },
+  handleClick: function(divid) {
+  	var newdivid = divid +'div';
+	if ($('#'+newdivid).hasClass('nodisplay')){
+    	$('#'+newdivid).removeClass('nodisplay');
+    	$('#'+divid+'link').html(' [-] ');
+    }else{
+    	$('#'+newdivid).addClass('nodisplay');
+    	$('#'+divid+'link').html(' [+] ');
+    }
+  },
 
   render: function() {
 
     var patientVisitHistoryList = this.props.patientVisitHistories.map(function(patientVisitHistory, index) {
+	var i =index+ 'link';
+	var divid =index+ 'div';
       return (
         <div key={index} className="list-group-item">
           <dt>{patientVisitHistory.dateString} : {patientVisitHistory.physician}</dt>
-          <dl>{patientVisitHistory.notes}</dl>
+          <dl>{patientVisitHistory.brief}
+         	<span className='linkClass' onClick={this.handleClick.bind(this,index)} id={i}>[+]</span>
+		  </dl>
+          <div id={divid} className='nodisplay'>
+          	<p>{patientVisitHistory.notes}</p>
+          </div>
         </div>
       );
-    });
+    }, this);
 
     return (
 
